66d4de0cebad04451ac30cc2244add6ce9e380cf
TheChyz <mchyziak@sfu.ca>
2018-05-26 10:00:05 -0700

    HW2 - terminal inputs finished

--- homework_2/visual_quicksort.cc
+++ homework_2/visual_quicksort.cc
@@ -25,13 +25,14 @@
 using namespace std;
 
 void drawObjects();
-void keyboardSpecial(int key, int x, int y);
 void drawCube(float startX, float startY, float startZ, 
 		float sizeX, float sizeY, float sizeZ,
-		float angleX, float angleY, float angleZ);
+		float angleX, float angleY, float angleZ,
+		float colourRed, float colourGreen, float colourBlue);
 void cubeSort(int argc, char **argv);
 void nameSort(const char* name_unsorted);
 void quickSort(int array[], int left_start, int right_start);
+int inputToInt(string user_input);
 
 // The current object being shown on the screen
 enum screenObject {all, cube};
@@ -123,10 +124,55 @@ void nameSort(const char* name_unsorted) {
 // TODO
 void cubeSort(int argc, char **argv) {
 
+	// Variables
+	int cube_colours[4][3];
+	int cube_index;
+	int colour_index;
+	string colour_string;
+	string user_input;
+	int user_input_int;
+
+	// Example explanation/initialization
+	printf("Four cubes will be shown on screen with custom RGB values.\n");
+	printf("You will be asked to input the RGB values next.\n");
+	printf("The intensity of a colour can be input from 0 to 100.\n");
+	
+	// Get user input
+	for (cube_index = 0; cube_index < 4; cube_index++) {
+		for (colour_index = 0; colour_index < 3; colour_index++) {
+			if (colour_index == 0) {
+				colour_string = "red";
+			}
+			else if (colour_index == 1) {
+				colour_string = "green";
+			}
+			else {
+				colour_string = "blue";
+			}
+			printf("Cube %d (%s intensity): ", cube_index + 1, 
+				colour_string.c_str());
+			cin >> user_input;
+
+			// Check if user input is a number between 0 and 100
+			user_input_int = inputToInt(user_input);
+			if (user_input_int >= 0 && 
+				user_input_int <= 100) {
+				cube_colours[cube_index][colour_index] = 
+					user_input_int;
+			}
+			else {
+				// Invalid input
+				printf("Error: invalid user input. Try "
+					"again.\n");
+				colour_index--;
+			}
+		}
+	}
+
 	// Initialize GLUT and process user parameters
 	glutInit(&argc, argv);
 
-	// Request double buffered true color window with Z-buffer
+	// Request double buffered true colour window with Z-buffer
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 
 	// Create window
@@ -143,13 +189,42 @@ void cubeSort(int argc, char **argv) {
 
 	// Callback functions
 	glutDisplayFunc(drawObjects);
-	glutSpecialFunc(keyboardSpecial);
 
 	// Pass control to GLUT for events
         glutMainLoop();
 }
 
-// TODO
+// Checks if a string can be converted to integer values.
+// Only integer values from 0 to 100 are accepted and then returned.
+// Otherwise -1 will be returned.
+int inputToInt(string user_input) {
+	
+	// Variables
+	int input_index;
+
+	// Check if input more than 3 characters then return -1
+	if (user_input.length() > 3) {
+		return -1;	
+	}
+	
+	// Check if input is a valid number
+	// Valid number is ascii value from 48 to 57
+	// Return -1 if not valid
+	for (input_index = 0; input_index < user_input.length();
+		input_index++) {
+		if ((user_input.at(input_index) < 48) ||
+			(user_input.at(input_index) > 57)) {
+			return -1;
+		}
+	}
+
+	// Convert valid string in integer format to integer
+	return stoi(user_input, nullptr);
+}
+
+// Uses the quicksort algorithm on an integer array with a left and right
+// starting point.
+// Used recursively and stores the final "sorted" array in the array given
 void quickSort(int array[], int left_start, int right_start) {
 	
 	// Variables
@@ -186,43 +261,10 @@ void quickSort(int array[], int left_start, int right_start) {
 
 }
 
-
-// Gets the user special input and changes the current object shown
-// The scene cycles between all 3 cube, pyramid, and diamond being shown (all)
-// The sequence is all -> cube -> pyramid -> diamond -> all for right arrow
-// The sequence is all -> diamond -> pyramid -> cube -> all for left arrow
-void keyboardSpecial(int key, int x, int y) {
-
-	// Checks what key is pressed and changes the current object 
-	// shown on screen if it is the left or right arrow
-	if (key == GLUT_KEY_RIGHT) {
-		switch(currentObject) {
-			case all:
-				currentObject = cube;
-				break;
-			case cube:
-				currentObject = all;
-				break;
-		}
-	}
-	else if (key == GLUT_KEY_LEFT) {
-		switch(currentObject) {
-			case all:
-				currentObject = cube;
-				break;
-			case cube:
-				currentObject = all;
-				break;
-		}
-	}
-
-	// Request display to update
-	glutPostRedisplay();
-
-}
-
 // Draws the cube, pyramid, and diamond
 void drawObjects() {
+
+	// TODO
 
 	// Set Background Color to a light grey
 	glClearColor(0.4, 0.4, 0.4, 1.0);
@@ -236,21 +278,23 @@ void drawObjects() {
 
 	// Draw cube, pyramid, and/or diamond based on current selection
 	if (currentObject == all) {
-		drawCube(-0.6, 0, 0, 0.4, 0.4, 0.4, -20, 20, 0);
+		drawCube(-0.6, 0, 0, 0.4, 0.4, 0.4, -20, 20, 0, 1.0, 0, 0);
 	}
 	else if (currentObject == cube) {
-		drawCube(0, 0, 0, 0.8, 0.8, 0.8, -20, 20, 0);
+		drawCube(0, 0, 0, 0.8, 0.8, 0.8, -20, 20, 0, 1.0, 0, 0);
 	}
 
 	glFlush();
 	glutSwapBuffers();
 }
 
-// Draws a red cube according to the size and location parameters
+// Draws a cube according to the size and location parameters
+// Color is also based on the set parameters
 // start location is middle of cube
 void drawCube(float startX, float startY, float startZ, 
 		float sizeX, float sizeY, float sizeZ,
-		float angleX, float angleY, float angleZ) {
+		float angleX, float angleY, float angleZ,
+		float colourRed, float colourGreen, float colourBlue) {
 	
 	float dx = sizeX/2;
 	float dy = sizeY/2;
@@ -265,7 +309,7 @@ void drawCube(float startX, float startY, float startZ,
 
 	// Front
     	glBegin(GL_POLYGON);
-    	glColor3f(1.0, 0.0, 0.0);
+    	glColor3f(colourRed, colourGreen, colourBlue);
         glVertex3f(startX-dx, startY+dy, startZ-dz);
         glVertex3f(startX+dx, startY+dy, startZ-dz);
         glVertex3f(startX+dx, startY-dy, startZ-dz);
@@ -274,7 +318,7 @@ void drawCube(float startX, float startY, float startZ,
 
 	// Left
         glBegin(GL_POLYGON);
-        glColor3f(0.9, 0.0, 0.0);
+    	glColor3f(colourRed, colourGreen, colourBlue);
         glVertex3f(startX-dx, startY+dy, startZ+dz);
         glVertex3f(startX-dx, startY+dy, startZ-dz);
         glVertex3f(startX-dx, startY-dy, startZ-dz);
@@ -283,7 +327,7 @@ void drawCube(float startX, float startY, float startZ,
 
         // Right
         glBegin(GL_POLYGON);
-        glColor3f(0.8, 0.0, 0.0);
+    	glColor3f(colourRed, colourGreen, colourBlue);
         glVertex3f(startX+dx, startY+dy, startZ+dz);
         glVertex3f(startX+dx, startY+dy, startZ-dz);
         glVertex3f(startX+dx, startY-dy, startZ-dz);
@@ -292,7 +336,7 @@ void drawCube(float startX, float startY, float startZ,
 
   	// Top
         glBegin(GL_POLYGON);
-        glColor3f(0.7, 0.0, 0.0);
+    	glColor3f(colourRed, colourGreen, colourBlue);
         glVertex3f(startX+dx, startY+dy, startZ+dz);
         glVertex3f(startX+dx, startY+dy, startZ-dz);
         glVertex3f(startX-dx, startY+dy, startZ-dz);
@@ -301,7 +345,7 @@ void drawCube(float startX, float startY, float startZ,
 
         // Bottom
         glBegin(GL_POLYGON);
-        glColor3f(0.6, 0.0, 0.0);
+    	glColor3f(colourRed, colourGreen, colourBlue);
         glVertex3f(startX+dx, startY-dy, startZ+dz);
         glVertex3f(startX+dx, startY-dy, startZ-dz);
         glVertex3f(startX-dx, startY-dy, startZ-dz);
@@ -310,7 +354,7 @@ void drawCube(float startX, float startY, float startZ,
 
 	// Back
         glBegin(GL_POLYGON);
-        glColor3f(0.5, 0.0, 0.0);
+    	glColor3f(colourRed, colourGreen, colourBlue);
         glVertex3f(startX-dx, startY+dy, startZ+dz);
         glVertex3f(startX+dx, startY+dy, startZ+dz);
         glVertex3f(startX+dx, startY-dy, startZ+dz);
